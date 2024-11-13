124e5973203fb54670c18b96ff1d34be5d569362
Abdul Aljawaab <abdulkhamis4041@gmail.com>
2024-11-13 08:59:41 +0300

    frstcodeof_tabhead_tblrow_db.json

---
+++ src/Buttons.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Buttons() {  
+    return (  
+      <>  
+        <button>A</button>  
+        <button>P</button>  
+      </>  
+    );  
+  } 
+
+export default Buttons
---
+++ src/TableRow.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function TableRow(){
+    return(
+        <tr>
+            <td>Name</td>
+            <td><Buttons /></td>
+            <td><Buttons /></td>
+            <td><Buttons /></td>
+            <td><Buttons /></td>
+            <td><Buttons /></td>
+        </tr>
+
+    )
+}
